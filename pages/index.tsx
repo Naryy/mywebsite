6eafe53824c61e2089c0a8753f65353913117fae
Naryy <narikawanariyuki@gmail.com>
2022-07-02 23:26:22 +0900

    index 0702

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.scss'
 import {Swiper, SwiperSlide} from 'swiper/react'
+import InstagramIcon from '/public/IG_Glyph_Fill.png'
 import SwiperCore, {Pagination, Navigation,Autoplay} from 'swiper'
 SwiperCore.use([Pagination, Navigation, Autoplay])
 
@@ -31,13 +32,16 @@ const Home: NextPage = () => {
         <div className={styles.side}>
           <div className={styles.sidewrap}>
             <div>
-              <h1 className = {styles.sidetitle}>Nary</h1>
+              <h1 className = {styles.sidetitle}><Link href="/">Nary</Link></h1>
             </div>
             <div>
               <p className = {styles.sidecontent}><Link href="./website"><a>Website</a></Link></p>
             </div>
             <div>
               <p className = {styles.sidecontent}><Link href="./picture"><a>Picture</a></Link></p>
+            </div>
+            <div className={styles.instagramIcon}>
+              <p><Link href="https://www.instagram.com/y.nary/"><a target="_blank"><Image src={InstagramIcon} alt="logo" className={styles.instaicon} width="30px" height="30px"></Image></a></Link></p>
             </div>
           </div>
         </div>
